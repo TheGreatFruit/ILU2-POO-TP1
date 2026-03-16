8db2e92e7c0c18339c06574e1eba0d2d1dc196f6
TheGreatFruit <pierre.28.gauthier@gmail.com>
2026-03-16 10:55:11 +0100

    Add next case

--- src/histoire/ScenarioCasDegrade.java
+++ src/histoire/ScenarioCasDegrade.java
@@ -6,5 +6,6 @@ import villagegaulois.Etal;
 public static void main(String[] args) {
   Etal etal = new Etal();
   etal.libererEtal();
+  etal.acheterProduit(1,null);
   System.out.println("Fin du test");
 }
